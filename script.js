4db5865cdba306943470bbf7e1007d653da9f2fc
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2025-06-22 23:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-function mincost(arr)
-{ 
-let cost = 0 ;
-while(arr.length > 1){
-    arr.sort((a,b)=> a-b);
+function mincost(arr) {
+    let cost = 0;
 
-  let first = arr.shift();
-  let second = arr.shift();
+    while (arr.length > 1) {
+        arr.sort((a, b) => a - b);
 
+        let first = arr.shift();
+        let second = arr.shift();
 
-  let newRope = first + second;
-  cost += newRope;
+        let newRope = first + second;
+        cost += newRope;
 
+        arr.push(newRope);
+    }
 
-  arr.push(newRope);
-
-  
+    return cost;
 }
 
-return cost;
+// Sample array
+let arr = [4, 3, 2, 6];
+let result = mincost(arr);
 
-}
-
-
-document.writeln(mincost(arr));
-module.exports=mincost;
+// Show result in the HTML
+document.getElementById('result').innerText =
+   result;
