0a93379076c2d1bc76085ac95f20b090a48ff031
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2025-06-22 22:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+let cost = 0 ;
+while(arr.length > 1){
+    arr.sort((a,b)=> a-b);
+
+  let first = arr.shift();
+  let second = arr.shift();
+
+
+  let newRope = first + second;
+  cost += newRope;
+
+
+  arr.push(newRope);
+
   
 }
 
+return cost;
+
+}
+console.log(mincost(arr));
+
+ // document.write("<h2>Minimum cost to connect ropes is: " + result + "</h2>");
 module.exports=mincost;
